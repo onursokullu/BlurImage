95925d40b38bca5a9c7414324d6eefcdd8fd47b9
onursokullu <sokulluonur@hotmail.com>
2019-06-19 18:12:15 +0300

    First

---
+++ BlurImage/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace BlurImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picBoxBig = new System.Windows.Forms.PictureBox();
+            this.picBoxSmall = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.MakeItBlur = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxBig)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxSmall)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBoxBig
+            // 
+            this.picBoxBig.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picBoxBig.Location = new System.Drawing.Point(371, 22);
+            this.picBoxBig.Name = "picBoxBig";
+            this.picBoxBig.Size = new System.Drawing.Size(299, 369);
+            this.picBoxBig.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBoxBig.TabIndex = 0;
+            this.picBoxBig.TabStop = false;
+            this.picBoxBig.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.picBoxBig.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.picBoxBig.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.picBoxBig.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // picBoxSmall
+            // 
+            this.picBoxSmall.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picBoxSmall.Location = new System.Drawing.Point(129, 22);
+            this.picBoxSmall.Name = "picBoxSmall";
+            this.picBoxSmall.Size = new System.Drawing.Size(100, 138);
+            this.picBoxSmall.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBoxSmall.TabIndex = 1;
+            this.picBoxSmall.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 231);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Blur Intensity";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(129, 184);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(86, 32);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Add Picture";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(529, 412);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(95, 37);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Save";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(77, 320);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // MakeItBlur
+            // 
+            this.MakeItBlur.Location = new System.Drawing.Point(401, 419);
+            this.MakeItBlur.Name = "MakeItBlur";
+            this.MakeItBlur.Size = new System.Drawing.Size(75, 30);
+            this.MakeItBlur.TabIndex = 13;
+            this.MakeItBlur.Text = "Blur";
+            this.MakeItBlur.UseVisualStyleBackColor = true;
+            this.MakeItBlur.Click += new System.EventHandler(this.MakeItBlur_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(125, 222);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 14;
+            this.trackBar1.Scroll += new System.EventHandler(this.updateBlur);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(691, 461);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.MakeItBlur);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.picBoxSmall);
+            this.Controls.Add(this.picBoxBig);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxBig)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxSmall)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBoxBig;
+        private System.Windows.Forms.PictureBox picBoxSmall;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button MakeItBlur;
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ BlurImage/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BlurImage
+{
+    public partial class Form1 : Form
+    {
+        Rectangle rect;
+        Point LocationXY;
+        Point LocationX1Y1;
+        bool IsMouseDown = false;
+        int blurIntensity=3;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void hScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.jfif, *.png) | *.jpg; *.jpeg; *.jpe; *.jfif; *.png";
+            if (openFileDialog1.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+            {
+                return;
+
+            }
+    
+            picBoxSmall.ImageLocation = openFileDialog1.FileName;
+            picBoxBig.ImageLocation = openFileDialog1.FileName;
+
+            //  btnConvertGray.PerformClick();
+
+
+
+        }
+       
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //private void picBoxBig_Click_1(object sender, EventArgs e)
+        //{
+          
+        //    this.picBoxBig.Name = "picBoxBig";
+            
+        //    this.picBoxBig.Click += new System.EventHandler(this.picBoxBig_Click_1);
+        //    string labelTex =string.Format("X: {0} X+1: {1} Y: {2} ", MousePosition.X, MousePosition.X+1, MousePosition.Y);
+        //    label5.Text = "You choiced the pixel coordinated"+labelTex;
+
+        //    float cons = 1 / 9;
+
+
+
+           
+        //    for (int i = MousePosition.X - 1; i <= MousePosition.X + 1; i++)
+        //    {
+        //        for (int j = MousePosition.Y - 1; j <= MousePosition.Y + 1; j++)
+        //        {
+        //            Console.Write("i: {0} j: {1}", i, j);
+
+
+
+        //        }
+        //    }
+
+        //}
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MakeItBlur_Click(object sender, EventArgs e)
+        {
+            Bitmap newBitmap = new Bitmap(picBoxBig.Image);
+            for (int x = LocationXY.X; x <= LocationX1Y1.X-1; x++)
+            {   
+                for (int y = LocationXY.Y; y <= LocationX1Y1.Y; y++)
+                {
+                    try
+                    {
+                        Color prevX = newBitmap.GetPixel(x - blurIntensity, y);
+                        Color nextY = newBitmap.GetPixel(x + blurIntensity, y);
+                        Color prevY = newBitmap.GetPixel(x, y - blurIntensity);
+                        Color nextX = newBitmap.GetPixel(x , y + blurIntensity);
+                        int avgR = (int)((prevX.R + nextY.R + prevY.R + nextX.R) / 4);
+                        int avgG = (int)((prevX.G + nextY.G + prevY.G + nextX.G) / 4);
+                        int avgB = (int)((prevX.B + nextY.B + prevY.B + nextX.B) / 4);
+                        newBitmap.SetPixel(x, y, Color.FromArgb(avgR, avgG, avgB));
+                       
+                      
+                    }
+                    catch (Exception) { }
+                }
+
+            }
+            picBoxBig.Image = newBitmap;
+
+
+        }
+
+        private void updateBlur(object sender, EventArgs e)
+        {
+
+            blurIntensity =int.Parse( trackBar1.Value.ToString()); 
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "Images|*.png;*.bmp;*.jpg";
+            ImageFormat format = ImageFormat.Png;
+            if (save.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string ext = System.IO.Path.GetExtension(save.FileName);
+                switch (ext)
+                {
+                    case ".jpg":
+                        format = ImageFormat.Jpeg;
+                        break;
+                    case ".bmp":
+                        format = ImageFormat.Bmp;
+                        break;
+                }
+                picBoxBig.Image.Save(save.FileName, format);
+            }
+        }
+
+      
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            IsMouseDown = true;
+            LocationXY = e.Location;
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (IsMouseDown == true)
+            {
+                LocationX1Y1 = e.Location;
+                Refresh();
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (IsMouseDown == true)
+            {
+                LocationX1Y1 = e.Location;
+                IsMouseDown = false;
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            if (rect != null)
+            {
+                e.Graphics.DrawRectangle(Pens.Black, GetRect());
+
+            }
+        }
+        private Rectangle GetRect()
+        {
+            rect = new Rectangle();
+            rect.X = Math.Min(LocationXY.X, LocationX1Y1.X);
+            rect.Y = Math.Min(LocationXY.Y, LocationX1Y1.Y);
+            rect.Width = Math.Abs(LocationXY.X - LocationX1Y1.X);
+            rect.Height = Math.Abs(LocationXY.Y - LocationX1Y1.Y);
+            return rect;
+        }
+    }
+}
